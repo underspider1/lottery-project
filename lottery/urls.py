20dfa1b7d918acbdd4a5094ca40875fd63cdc3bc
underspider1 <itsbig@mail.ru>
2024-12-26 00:08:35 +0500

    1

--- lottery/urls.py
+++ lottery/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/', admin.site.urls),  # Include this line
     path('', views.home_view, name='home'),
     path('pull/<int:banner_id>/', views.pull_view, name='pull'), 
     path('inventory/', views.inventory_view, name='inventory'),
