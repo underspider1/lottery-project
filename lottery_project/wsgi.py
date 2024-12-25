1e8ce3a2043c558bd55d3435e417bf5ba2e15c64
underspider1 <itsbig@mail.ru>
2024-12-26 01:42:19 +0500

    1

--- lottery_project/wsgi.py
+++ lottery_project/wsgi.py
@@ -1,7 +1,24 @@
 import os
+import sys
+from pathlib import Path
+
+from dotenv import load_dotenv  #For loading .env file.
+
+load_dotenv() # Call this function here
+
+# Add the project and virtual environment paths
+BASE_DIR = Path(__file__).resolve().parent.parent
+PROJECT_ROOT = BASE_DIR.parent
+
+
+sys.path.append(str(PROJECT_ROOT))  # Add the parent directory
+sys.path.append(str(PROJECT_ROOT / "lottery_project"))
+sys.path.append(str(PROJECT_ROOT / "lottery_project/.venv/lib/python3.10/site-packages"))  # Add virtual env path. This line depends on where PythonAnywhere puts your virtual environment files.
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lottery_project.settings")  #PythonAnywhere might use some other settings, but this should also be fine
+
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lottery_project.settings")
 
 application = get_wsgi_application()
