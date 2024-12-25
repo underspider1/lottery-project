7f0d24bdfe09d83550f3aabbf025c0ac706c468b
underspider1 <itsbig@mail.ru>
2024-12-26 01:22:01 +0500

    2

--- lottery/urls.py
+++ lottery/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from lottery_project import views   # Absolute import
 
 urlpatterns = [
     path('pull/<int:banner_id>/', views.pull_view, name='pull'),
