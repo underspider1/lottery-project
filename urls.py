eb47f727db6b85352d7dcd36064f58db2b193e3a
underspider1 <itsbig@mail.ru>
2024-12-26 01:36:25 +0500

    2

--- urls.py
+++ urls.py
@@ -1,21 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include, reverse
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+from lottery_project import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home_view, name='home'), 
-    path('', include('lottery.urls')),
+    path('admin/', admin.site.urls),  # Admin site
+    path('', views.home_view, name='home'),  # Home view
+    path('', include('lottery.urls')),  # Include lottery app URLs
+    path('register/', views.register_view, name='register'), #Make sure this exists in lottery_project/views.py
+    path('login/', views.login_view, name='login'), #Make sure this exists in lottery_project/views.py
+    path('logout/', views.logout_view, name='logout'), #Make sure this exists in lottery_project/views.py
 ]
 
-def home_view(request):
-    first_banner = Banner.objects.filter(is_active=True).order_by('id').first()
-    if first_banner:
-        return redirect('pull', banner_id=first_banner.pk) # Here you need to provide a banner_id
-    else:
-         return redirect(reverse('register'))  # Redirect to registration page using its name
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:  # Serve media files only in DEBUG mode
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROO
